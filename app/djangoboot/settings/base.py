a813f0feb8c99161a2ecd66f9edbfcdc9868e872
Pablo Ricco <pricco@gmail.com>
2013-09-28 17:19:47 -0300

    social auth: associate by email

--- app/djangoboot/settings/base.py
+++ app/djangoboot/settings/base.py
@@ -137,6 +137,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_uid',
     'social.pipeline.social_auth.auth_allowed',
     'social.pipeline.social_auth.social_user',
+    'social.pipeline.social_auth.associate_by_email',
     'social.pipeline.user.get_username',
     'social.pipeline.user.create_user',
     'social.pipeline.social_auth.associate_user',
