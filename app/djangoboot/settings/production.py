032e6f20dfd2fa439121af184bf068cd4f8ca088
Pablo Ricco <pricco@gmail.com>
2013-09-28 13:46:56 -0300

    fix storage issue

--- app/djangoboot/settings/production.py
+++ app/djangoboot/settings/production.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = ['djangoboot.com']
 MEDIA_BUCKET = 'djangoboot-static'
 MEDIA_URL = 'http://d2n6mhm0pe75h0.cloudfront.net/'
 MEDIA_DOMAIN = 'd2n6mhm0pe75h0.cloudfront.net'
-DEFAULT_FILE_STORAGE = 'core.storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'core.storage.MediaStorage'
 
 
 STATIC_BUCKET = 'djangoboot-media'
 STATIC_URL = 'http://dhcgtpvyqjrjc.cloudfront.net/'
 STATIC_DOMAIN = 'dhcgtpvyqjrjc.cloudfront.net'
-STATICFILES_STORAGE = 'core.storages.MediaStorage'
+STATICFILES_STORAGE = 'core.storage.MediaStorage'
