210cfde980837e5aeee09506b8b45cf981079163
Pablo Ricco <pricco@gmail.com>
2013-09-27 22:12:35 -0300

    add bootstrap and home app

---
+++ app/djangoboot/settings/base.py
@@ -0,0 +1,119 @@
+import os
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
+
+
+def rel(*x):
+    return os.path.join(APP_ROOT, *x)
+
+try:
+    from secrets import *
+except ImportError:
+    pass
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Sebastian Nogara', 'snogaraleal@gmail.com'),
+    ('Pablo Ricco', 'pricco@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoboot',
+        'USER': 'sl',
+        'PASSWORD': 'sl',
+        'HOST': '',
+        'PORT': ''
+    }
+}
+
+ALLOWED_HOSTS = []
+
+
+TIME_ZONE = 'America/Chicago'
+
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+MEDIA_ROOT = rel('media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    rel('static')
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = 'a1gf&i7+&+p7lkuazmvvw4g(w5ce(=i=k!quypy%q8-!tx=3*8'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader'
+)
+TEMPLATE_DIRS = (
+    rel('templates')
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'djangoboot.urls'
+
+WSGI_APPLICATION = 'djangoboot.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+
+)
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
