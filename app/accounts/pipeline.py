ff3a6dfe6017d737977ea9da35df6175ff4298e6
Sebastián Nogara <snogaraleal@gmail.com>
2013-09-28 16:44:02 -0300

    Update social auth pipeline.

--- app/accounts/pipeline.py
+++ app/accounts/pipeline.py
@@ -3,4 +3,3 @@ def store_redirect(request, user=None, *args, **kwargs):
     next = request.GET.get('next')
     if user and user.is_authenticated():
         request.session['next'] = next or user.get_absolute_url()
-        print request.session['next']
