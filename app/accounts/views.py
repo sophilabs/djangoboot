0b3d3f4ab3984c00eef2d9a217522997b0b13a63
Sebastián Nogara <snogaraleal@gmail.com>
2013-09-28 09:49:06 -0300

    Add group views.

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.views.generic import CreateView, UpdateView
+
+from accounts.models import Group, User
+
+
+class GroupCreateView(CreateView):
+    model = Group
+
+
+class GroupUpdateView(UpdateView):
+    model = Group
+
+
+class GroupDeleteView(UpdateView):
+    model = Group
